af3046da6476df87f67e7c7c4d187d42a3d90c64
Iliyas Momin <95050094+IliyasMomin77@users.noreply.github.com>
2022-04-28 23:54:29 +0530

    Update AWSKey Backup code.py

--- AWSKey Backup code.py
+++ AWSKey Backup code.py
@@ -3,8 +3,8 @@ from datetime import datetime
 import os
 
 today = datetime.now()
-x='AWS-Keys_'
-source_dir = r"D:\AWS\Keys"
+x='AWS-Data_'
+source_dir = r"D:\AWS"
 destination_dir = r"C:\Users\Iliyas\OneDrive\Documents\_" + (x) + today.strftime('%Y%m%d_%H_%M_%S')
 shutil.copytree(source_dir, destination_dir)
 
